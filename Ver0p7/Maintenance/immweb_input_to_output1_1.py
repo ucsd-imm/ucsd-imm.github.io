08f63d15da95555ec64feb252ef743777b3a4139
rsaito <golgo8128@yahoo.co.jp>
2015-11-26 23:37:11 -0800

    Temporary page ...

---
+++ Ver0p7/Maintenance/immweb_input_to_output1_1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+'''
+Created on 2015/11/26
+
+@author: rsaito
+'''
+
+import os
+from glob import glob
+
+IMMWEB_TOPDIR   = "/Users/rsaito/Desktop/UCSD_IMM_ver0p7"
+REPLACE_STR     = "<!-- main_area content -->"
+TEMPLATE_FILE   = os.path.join(IMMWEB_TOPDIR, "TEMPLATES", "template1.html")
+INPUT_HTML_DIR  = os.path.join(IMMWEB_TOPDIR, "HTMLi")
+OUTPUT_HTML_DIR = os.path.join(IMMWEB_TOPDIR, "HTMLo")
+
+template_html_str = "".join(open(TEMPLATE_FILE).readlines())
+
+
+for ifile in glob(os.path.join(INPUT_HTML_DIR, "*.[Hh]tm*")):
+    html_istr = "".join(open(ifile).readlines())
+    ofile = os.path.join(OUTPUT_HTML_DIR, os.path.basename(ifile))
+    html_ostr = template_html_str.replace(REPLACE_STR, html_istr)
+
+    open(ofile, "w").write(html_ostr)        
+    print(ifile, "was converted to", ofile)
+
+    
